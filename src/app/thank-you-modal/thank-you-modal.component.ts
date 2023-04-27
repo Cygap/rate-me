f9df91b78ec10923c0c118b9d48275ad06188130
cygap <113382267+Cygap@users.noreply.github.com>
2023-04-27 23:14:40 +0200

    created thank-you component placeholder

---
+++ src/app/thank-you-modal/thank-you-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-thank-you-modal',
+  templateUrl: './thank-you-modal.component.html',
+  styleUrls: ['./thank-you-modal.component.css']
+})
+export class ThankYouModalComponent {
+
+}
